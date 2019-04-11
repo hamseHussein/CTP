61a8bbb512f21aac51817861749c7832e1f422b0
hamseHussein <hamseedits@gmail.com>
2019-04-11 10:18:04 +0100

    Add files via upload

---
+++ api/v1/web/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'threes11_hamse');
+define('DB_PASSWORD', '2362');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'threes11_users');
+
+?>
