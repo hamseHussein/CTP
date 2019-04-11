0314bdd3675c65b0380ff02fe5d351039b4d6a21
hamseHussein <hamseedits@gmail.com>
2019-04-11 09:09:44 +0100

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,42 @@
+var app = angular.module('authenticationApp', ['ngRoute', 'ngAnimate', 'toaster']);
+
+app.config(['$routeProvider',
+  function ($routeProvider) {
+        $routeProvider.
+        when('/login', {
+            title: 'Login',
+            templateUrl: 'views/login.html',
+            controller: 'login'
+        })
+            .when('/logout', {
+                title: 'Logout',
+                templateUrl: 'views/login.html',
+                controller: 'login'
+            })
+            .when('/signup', {
+                title: 'Signup',
+                templateUrl: 'views/signup.html',
+                controller: 'signup'
+            })
+            .when('/dashboard', {
+                title: 'Dashboard',
+                templateUrl: 'views/dashboard.html',
+                controller: 'dashboard'
+            })
+      
+            .when('/property', {
+                title: 'Property',
+                templateUrl: 'views/property.html',
+                controller: 'property'
+            })
+      
+            .when('/', {
+                title: 'Login',
+                templateUrl: 'views/login.html',
+                controller: 'login',
+                role: '0'
+            })
+            .otherwise({
+                redirectTo: '/login'
+            });
+  }]);
